f71ca42f97ba990cf568ddf4dfab2634a08aed36
张冬 <you@example.com>
2016-11-21 14:12:27 +0800

    初始化项目

---
+++ 百思不得姐/百思不得姐/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  百思不得姐
+//
+//  Created by 张冬 on 2016/11/21.
+//  Copyright © 2016年 张冬. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 百思不得姐/百思不得姐/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  百思不得姐
+//
+//  Created by 张冬 on 2016/11/21.
+//  Copyright © 2016年 张冬. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
