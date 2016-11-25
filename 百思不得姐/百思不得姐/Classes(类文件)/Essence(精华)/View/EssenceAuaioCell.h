fdf392701391abc891270715bc695675103cb5da
张冬 <you@example.com>
2016-11-25 16:19:48 +0800

    再优化了界面！

---
+++ 百思不得姐/百思不得姐/Classes(类文件)/Essence(精华)/View/EssenceAuaioCell.h
@@ -0,0 +1,20 @@
+//
+//  EssenceAuaioCell.h
+//  百思不得姐
+//
+//  Created by 张冬 on 2016/11/22.
+//  Copyright © 2016年 张冬. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class BDJEssenceDetail;
+@interface EssenceAuaioCell : UITableViewCell
+
+//数据
+@property (nonatomic,strong)BDJEssenceDetail *detailModel;
+
+//便利才创建cell的方法
++ (EssenceAuaioCell *)audioCellForTableView:(UITableView *)tableView atIndexPath:(NSIndexPath *)indexpath withModel:(BDJEssenceDetail *)detailModel;
+
+@end
---
+++ 百思不得姐/百思不得姐/Classes(类文件)/Essence(精华)/View/EssenceImageCell.h
@@ -0,0 +1,20 @@
+//
+//  EssenceImageCell.h
+//  百思不得姐
+//
+//  Created by 张冬 on 2016/11/22.
+//  Copyright © 2016年 张冬. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class BDJEssenceDetail;
+@interface EssenceImageCell : UITableViewCell
+
+//数据
+@property (nonatomic,strong)BDJEssenceDetail *detailModel;
+
+//便利才创建cell的方法
++ (EssenceImageCell *)imageCellForTableView:(UITableView *)tableView atIndexPath:(NSIndexPath *)indexpath withModel:(BDJEssenceDetail *)detailModel;
+
+@end
